cea3ddab053b9e61ee4f950bd5bd9c7a1c88a120
Luca Ferroni <luca@befair.it>
2020-03-22 12:14:17 +0100

    Removed comments

--- main.tf
+++ main.tf
@@ -1,12 +1,3 @@
-# Set the variable value in *.tfvars file
-# or using the -var="hcloud_token=..." CLI option
-# variable "hcloud_token" {}
-
-# Configure the Hetzner Cloud Provider
-# provider "hcloud" {
-#  token = "${var.hcloud_token}"
-# }
-
 # Configure the Hetzner Cloud Provider
 provider "hcloud" {
   token = "YOUR HETZNER TOKEN HERE"
